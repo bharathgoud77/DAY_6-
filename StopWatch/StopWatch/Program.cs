dcedf6f5ad03d2f132d8768303d84b913a971da5
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-12-18 16:46:05 +0530

    Stop Watch

---
+++ StopWatch/StopWatch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace StopWatch
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            //Console.WriteLine("enter number:");
+            for (int i = 0; i < 1000; i++)
+            {
+                Thread.Sleep(5);
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Elapsed time:{0:hh\\:mm\\:ss}", stopwatch.Elapsed);
+
+        }
+    }
+}

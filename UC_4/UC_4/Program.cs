c50be1ab6b23471e032ffb1ba99a5146a8b1bcba
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-12-18 16:43:01 +0530

    Reverse Number

---
+++ UC_4/UC_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number:");
+            int num = int.Parse(Console.ReadLine());
+            int reverse = 0;
+            while (num != 0)
+            {
+                int rem = num % 10;
+                reverse = reverse * 10 + rem;
+                num = num / 10;
+            }
+            Console.WriteLine("Reverse of a number is:" + reverse);
+        }
+    }
+}
